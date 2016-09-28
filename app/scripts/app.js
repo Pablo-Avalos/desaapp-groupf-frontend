4048c858c67ebecd37540c79b7c7e87b669e0697
avalospablod <avalospablod@gmail.com>
2016-09-28 20:01:15 -0300

    scaffolding angular

---
+++ app/scripts/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name desaappGroupfFrontendApp
+ * @description
+ * # desaappGroupfFrontendApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('desaappGroupfFrontendApp', [
+    'ngResource'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name desaappGroupfFrontendApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the desaappGroupfFrontendApp
+ */
+angular.module('desaappGroupfFrontendApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
