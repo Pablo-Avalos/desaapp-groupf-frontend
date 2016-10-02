6e100d790158b0aa2cef79897176b20517f745d7
avalospablod <avalospablod@gmail.com>
2016-10-02 17:22:32 -0300

    web.js modificado

--- web.js
+++ web.js
@@ -5,3 +5,9 @@ var gzippo = require('gzippo');
   app.use(express.logger('dev'));
   app.use(gzippo.staticGzip("" + __dirname + "/dist"));
   app.listen(process.env.PORT || 5000);
+  
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
