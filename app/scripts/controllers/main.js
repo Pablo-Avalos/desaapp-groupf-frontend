be74432475c5edbd6050c6f93c366a34a5c26c86
avalospablod <avalospablod@gmail.com>
2016-10-25 19:25:14 -0300

    deploy de proyecto

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -6,12 +6,34 @@
  * @description
  * # MainCtrl
  * Controller of the desaappGroupfFrontendApp
- */
+ 
 angular.module('desaappGroupfFrontendApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+  .controller('myCtrl',function($scope, $http) {
+
+    
+    $http.get("http://desapp-groupf.herokuapp.com/rest/profile/getprofile/2")
+    .then(function(response) {
+        $scope.myPerfil = response.data;
+        console.log(myperfil);
+        })
+    ;}
+    
+  );
+  */
+  var urlBase = "https://desapp-groupf.herokuapp.com/rest/";
+  var app = angular.module('desaappGroupfFrontendApp', []);
+app.controller('myCtrl', function($scope, $http) {
+    $http({
+        method : "GET",
+        url : urlBase + "profile/getprofile/2",
+        headers: {'Content-Type': 'application/json'}
+    }).then(function mySucces(response) {
+        $scope.myPerfil = response.data.nik;
+        console.log(response);
+    }, function myError(response) {
+            console.log(response);
+
+        $scope.myPerfil = response.statusText.nik;
+    });
+});
+
